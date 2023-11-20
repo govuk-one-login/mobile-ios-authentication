67563d97f9af25cc7ffdf2275b6696f7d6caa5ff
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-11-20 19:09:20 +0000

    test: adding test for httpbody of TokenRequest

--- Tests/AuthenticationTests/URLRequest+ExtensionsTests.swift
+++ Tests/AuthenticationTests/URLRequest+ExtensionsTests.swift
@@ -4,11 +4,12 @@ import XCTest
 class URLRequestExtensionsTests: XCTestCase {
     func test_tokenRequestURL() throws {
         let authorizationCode = "123456789"
-        let requestBody = TokenRequest(authorizationCode: authorizationCode, redirectURI: "1234").formEncoded
+        let requestBody = TokenRequest(authorizationCode: authorizationCode, redirectURI: "123456789").formEncoded
         let url = URL(string: "https://www.google.com/token")!
         let sut = URLRequest.tokenRequest(body: requestBody, endpoint: url)
         XCTAssertEqual(sut.url, url)
         XCTAssertEqual(sut.httpMethod, "POST")
         XCTAssertEqual(sut.value(forHTTPHeaderField: "Content-Type"), "application/x-www-form-urlencoded")
+        XCTAssertEqual(sut.httpBody, "grant_type=authorization_code&code=123456789&redirect_uri=123456789".data(using: .utf8))
     }
 }
