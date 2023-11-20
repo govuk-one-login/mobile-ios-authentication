fb2b9bfe6a1ab3ed50ae9fbcd376c58dc1a32213
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-11-20 19:33:11 +0000

    fix: removing the need to serialise TokenRequest into JSON

--- Sources/Authentication/Custom/TokenRequest.swift
+++ Sources/Authentication/Custom/TokenRequest.swift
@@ -1,17 +1,10 @@
 import Foundation
 
-struct TokenRequest: Codable {
+struct TokenRequest {
     let authorizationCode: String
-    let grantType: String = "authorization_code"
     let redirectURI: String
     
-    enum CodingKeys: String, CodingKey {
-        case authorizationCode = "code"
-        case grantType = "grant_type"
-        case redirectURI = "redirect_uri"
-    }
-    
     var formEncoded: Data {
-        "grant_type=\(grantType)&code=\(authorizationCode)&redirect_uri=\(redirectURI)".data(using: .utf8)!
+        "grant_type=authorization_code&code=\(authorizationCode)&redirect_uri=\(redirectURI)".data(using: .utf8)!
     }
 }
