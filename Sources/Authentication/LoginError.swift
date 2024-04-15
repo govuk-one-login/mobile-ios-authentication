012e6c092f8cfc98689cc6d3834afe1d289daf97
Dylan James <90717175+DylanRJ@users.noreply.github.com>
2024-04-15 15:48:47 +0100

    fix: resolving dependencies and adding localized description to LoginError (#32)
    * chore: resolving package dependency versions
    
    * chore: adding localized description to LoginError
    
    * chore: standardising runner

--- Sources/Authentication/LoginError.swift
+++ Sources/Authentication/LoginError.swift
@@ -5,4 +5,21 @@ public enum LoginError: Error, Equatable {
     case network
     case non200
     case userCancelled
+    
+    var localizedDescription: String {
+        switch self {
+        case .clientError:
+            return "client error"
+        case .generic(description: let description):
+            return description
+        case .invalidRequest:
+            return "invalid request"
+        case .network:
+            return "network"
+        case .non200:
+            return "non 200"
+        case .userCancelled:
+            return "user cancelled"
+        }
+    }
 }
---
+++ Tests/AuthenticationTests/LoginErrorTests.swift
@@ -0,0 +1,23 @@
+@testable import Authentication
+import XCTest
+
+final class LoginErrorTests: XCTestCase {
+    var sut: LoginError!
+}
+
+extension LoginErrorTests {
+    func test_localizedDescription() throws {
+        sut = .clientError
+        XCTAssertEqual(sut.localizedDescription, "client error")
+        sut = .generic(description: "generic error")
+        XCTAssertEqual(sut.localizedDescription, "generic error")
+        sut = .invalidRequest
+        XCTAssertEqual(sut.localizedDescription, "invalid request")
+        sut = .network
+        XCTAssertEqual(sut.localizedDescription, "network")
+        sut = .non200
+        XCTAssertEqual(sut.localizedDescription, "non 200")
+        sut = .userCancelled
+        XCTAssertEqual(sut.localizedDescription, "user cancelled")
+    }
+}
