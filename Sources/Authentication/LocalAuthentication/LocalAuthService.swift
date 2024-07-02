79d3600b402a25cce5eb63168fb167d20f838553
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-07-02 14:45:08 +0100

    feat: adding LocalAuthService protocol

---
+++ Sources/Authentication/LocalAuthentication/LocalAuthService.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+public enum AuthType {
+    case none
+    case face
+    case finger
+    case pass
+}
+
+public protocol LocalAuthService {
+    func hasLocalAuthSetup(navigationController: UINavigationController) -> AuthType
+}
