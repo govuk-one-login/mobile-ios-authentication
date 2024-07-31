fba00695f25ada1545ea4f70de74e7679e6c0f67
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-07-31 14:37:13 +0100

    feat: adding file, function and line to waitFortruth

--- Tests/AuthenticationTests/XCTestCase+Extensions.swift
+++ Tests/AuthenticationTests/XCTestCase+Extensions.swift
@@ -3,7 +3,10 @@ import XCTest
 
 extension XCTestCase {
     func waitForTruth(_ expression: @autoclosure @escaping () -> Bool,
-                      timeout: TimeInterval) {
+                      timeout: TimeInterval,
+                      file: String = #fileID,
+                      function: String = #function,
+                      line: Int = #line) {
         let exp = expectation(for: .init { _, _ in
             expression()
         }, evaluatedWith: nil, handler: nil)
