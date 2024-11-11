afe93a6820b375582df49267e1af41ed26614cf6
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-11-11 12:56:12 +0000

    fix: linting again

--- Sources/Authentication/AppAuthSession.swift
+++ Sources/Authentication/AppAuthSession.swift
@@ -30,7 +30,7 @@ public final class AppAuthSession: LoginSession {
         )
     }
     
-    // swiftlint:disable:next function_body_length
+    // swiftlint:disable: function_body_length
     /// This is here for testing and allows `service` to be mocked
     @MainActor
     func performLoginFlow(
@@ -97,6 +97,7 @@ public final class AppAuthSession: LoginSession {
             }
         }
     }
+    // swiftlint:enable: function_body_length
     
     /// Ensures `finalise` is public and can be called by the app
     /// Handles the redirect URL from the login modal
--- Tests/AuthenticationTests/Mocks/PresentErrors/AuthorizationInvalidRequest/MockOIDAuthorizationService_AuthorizationInvalidRequest.swift
+++ Tests/AuthenticationTests/Mocks/PresentErrors/AuthorizationInvalidRequest/MockOIDAuthorizationService_AuthorizationInvalidRequest.swift
@@ -1,6 +1,7 @@
 import AppAuthCore
 import UIKit
 
+// swiftlint:disable:next type_name
 public class MockOIDAuthorizationService_AuthorizationInvalidRequest: OIDAuthorizationService {
     public override class func present(
         _ request: OIDAuthorizationRequest,
